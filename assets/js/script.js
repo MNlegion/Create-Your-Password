06789eacbce0aa99c744ac269a4573eccd7d3b24
MNlegion <rkreuser30@gmail.com>
2022-03-27 14:46:17 -0500

    added final comments within script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-// Assignment code here
 
-// Array and values
+// Array and specified values
 upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 numericValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 specialCharacter = ["+", "-", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":"]
-// Will I run into issues with this being a string and the " and \ symbols? I tried to call them out correctly using the slash before inputting the symbol itself?
+
 
 function generatePassword() {
+    // arrayIndex is used to store confirm input if value = true
     var arrayIndex = []
     var charLimit = parseInt(window.prompt("Passwords may contain between 8 and 128 characters. Please determine the length of your password."));
     if (charLimit > 128 || charLimit < 8 || charLimit === "" || charLimit === null) {
@@ -16,6 +16,7 @@ function generatePassword() {
     }
     console.log(charLimit);
 
+    // Confirm user inputs to specify which arrays above are eligible for entry
     var upperConfirm = window.confirm("Do you want your password to contain uppercase characters?");
     console.log(upperConfirm);
     var lowerConfirm = window.confirm("Do you want your password to contain lowercase characters?");
@@ -42,9 +43,8 @@ function generatePassword() {
     }
     console.log("Here are the user input selections and outputs", arrayIndex)
 
-    //  Not sure the below is needed, it creates one large array of 80 characters
+    // totalSet is used to distribute the filler characters in order to meet specified password length by concatenating TRUE array elements within arrayIndex
     var totalSet = [].concat.apply([], arrayIndex);
-    //   console.log("whats this do?", merge)
     function randomIndex(length) {
         return Math.floor(Math.random() * length);
     }
@@ -57,6 +57,7 @@ function generatePassword() {
         console.log(result);
     }
 
+    // Helps to determine the remaining total characters to place within the password after applying at least 1 of the selected user input confirms
     var remainder = charLimit - arrayIndex.length;
     console.log(remainder);
     for (var i = 0; i < remainder; i++) {
@@ -64,34 +65,7 @@ function generatePassword() {
         console.log(result);
     }
     return result
-    // For each window.Confirm True (var i = true), select minimum of 1 element from array. 
-    // how to select 1 element from each true index of the arrayIndex vs returning the first X elements from the [0] index?
-    // out loop - to iterate between outer arrays
-    // inner loop - to interate between inner elements
-    // merge[rowIndex][columnIndex]
-    // Once each True array has a minimum of 1 element selected, randomize remaining elements from available True array elements until charLimit is defined. 
-
-    // arrayPassword.push(totalSet[Math.floor(Math.random() * totalSet.length)]);
-    // For Loop ends
-
-    // password = array_password.join("");
 }
-
-
-
-// Reset Password Function - Work in Progress
-
-// var resetPassword = window.confirm("Would you like to creeate a new password?");
-
-// if (resetPassword) {
-//   generatePassword();
-// } else {
-//   window.alert("Thank you for trying out my password generator. Come back again!");
-// };
-
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
